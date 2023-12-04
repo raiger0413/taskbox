b4fed7ca9978800c8276ea0297345b7c34afa327
raiger marcano <raigermarcano0413@gmail.com>
2023-12-04 16:08:10 -0500

    taskbox

--- taskbox/src/app/app.component.ts
+++ taskbox/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
- template: `
-   <app-inbox-screen></app-inbox-screen>
- `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'taskbox';
